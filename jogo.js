140304adc4b56de48a8796cc29dbda12a40a83ca
João Negreiros <5104767-Joao_Negreiros@users.noreply.gitlab.com>
2021-03-09 11:32:19 -0300

    Commit inicial do projeto

---
+++ jogo.js
@@ -0,0 +1,100 @@
+console.log('Flappy Bird')
+
+const sprites = new Image()
+sprites.src = './sprites.png'
+
+const canvas = document.querySelector('canvas')
+const contexto = canvas.getContext('2d')
+
+// Objeto que tem as caracteristicas do flappy bird a serem desenhadas:
+const flappyBird = {
+    spriteX: 0, // Sx e Sy (search X e Y) do elemento que vamos pegar de dentro da img
+    spriteY: 0, 
+    largura: 33, // SWidth e SHeight, tamanho do recorte na sprite
+    altura: 24,
+    x: 10, // Dx e Dy (Draw x e y) - coordenadas a serem usadas no canvas
+    y: 50,
+    gravidade: 0.25,
+    velocidade: 0,
+    atualiza(){
+        flappyBird.velocidade += flappyBird.gravidade
+        flappyBird.y += flappyBird.velocidade
+    },
+    desenhar(){ // f(x) que desenha ele mesmo
+        contexto.drawImage(
+            sprites, // Local onde está a imagem de referencia
+            flappyBird.spriteX, flappyBird.spriteY, 
+            flappyBird.largura, flappyBird.altura, 
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura
+        )
+    }
+}
+
+const chao = {
+    spriteX: 0,
+    spriteY: 610, 
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height - 112,
+    desenhar(){
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY, 
+            chao.largura, chao.altura, 
+            chao.x, chao.y,
+            chao.largura, chao.altura
+        )
+        
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY, 
+            chao.largura, chao.altura, 
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura
+        )
+    }
+}
+
+const planoDeFundo = {
+    spriteX: 390,
+    spriteY: 0, 
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
+    desenhar(){
+        // desenha um quadrado com fundo '#70c5ce' que pegue toda a extenção do canvas 
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0,0, canvas.width, canvas.height)
+
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY, 
+            planoDeFundo.largura, planoDeFundo.altura, 
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura
+        )
+        
+        // faz com que o chão cubra toda a largura do canvas
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY, 
+            planoDeFundo.largura, planoDeFundo.altura, 
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura
+        )
+    }
+}
+
+// --------------------------------Desenhar------------------------------------------
+function loop(){
+    flappyBird.atualiza()
+    planoDeFundo.desenhar()
+    chao.desenhar()
+    flappyBird.desenhar()
+
+    requestAnimationFrame(loop) // Função para criar um loop que forma os FPS na tela
+}
+loop()
