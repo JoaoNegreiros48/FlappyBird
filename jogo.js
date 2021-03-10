ae433bb49c7d1134cc21b133a42b78f17aecb14f
João Negreiros <5104767-Joao_Negreiros@users.noreply.gitlab.com>
2021-03-10 10:47:40 -0300

    Desenhando obstáculos dinamicamente, detectando Colisão,  Cenário infinito e animando o passarinho com frames

--- jogo.js
+++ jogo.js
@@ -1,7 +1,8 @@
-console.log('Flappy Bird')
-
+// --------------------------------- Variáveis -----------------------------
 const sprites = new Image()
 sprites.src = './sprites.png'
+
+let frame = 0
 
 const somHit = new Audio()
 somHit.src = './efeitos/efeitos_hit.wav'
@@ -9,9 +10,15 @@ somHit.src = './efeitos/efeitos_hit.wav'
 const canvas = document.querySelector('canvas')
 const contexto = canvas.getContext('2d')
 
+const globais = {} // objeto que preenche tudo que for global
+// Telas contem as funções que cada tela ira usar
+let telaAtiva = {}
+
+// --------------------------------- Funções -----------------------------
+
 function fazColisao(){
  const flappyBirdY = globais.flappyBird.y + globais.flappyBird.altura
- const chaoY = chao.y
+ const chaoY = globais.chao.y
 
  if(flappyBirdY >= chaoY){
     return true
@@ -19,9 +26,8 @@ function fazColisao(){
  return false
 }
 
-// Objeto que tem as caracteristicas do flappy bird a serem desenhadas:
 function criaFlappyBird(){
-    const flappyBird = {
+    const flappyBird = { // Objeto que tem as caracteristicas do flappy bird a serem desenhadas:
         spriteX: 0, // Sx e Sy (search X e Y) do elemento que vamos pegar de dentro da img
         spriteY: 0, 
         largura: 33, // SWidth e SHeight, tamanho do recorte na sprite
@@ -30,13 +36,13 @@ function criaFlappyBird(){
         y: 50,
         pulo: 4.6,
         pula(){
-            console.log('pular')
+            console.log('pulos')
             flappyBird.velocidade = - flappyBird.pulo
         },
         gravidade: 0.25,
         velocidade: 0,
         atualiza(){
-            if(fazColisao(flappyBird, chao)){
+            if(fazColisao(flappyBird, globais.chao)){
                 somHit.play()
                 setTimeout(() => {
                     mudarTela(telas.inicio)
@@ -48,10 +54,30 @@ function criaFlappyBird(){
             flappyBird.velocidade += flappyBird.gravidade
             flappyBird.y += flappyBird.velocidade
         },
+        movimentos: [
+            { spriteX: 0, spriteY: 0, }, // asa pra cima
+            { spriteX: 0, spriteY: 26, }, // asa no meio 
+            { spriteX: 0, spriteY: 52, }, // asa pra baixo
+            { spriteX: 0, spriteY: 26, }, // asa no meio 
+        ],
+        frameAtual: 0,
+        atualizaFrameAtual() {
+            const intervaloFrames = 10
+            const passouIntervalo = frame % intervaloFrames === 0
+
+            if (passouIntervalo){
+                const baseIncremento = 1
+                const incremento = baseIncremento + flappyBird.frameAtual
+                const baseRepeticao = flappyBird.movimentos.length
+                flappyBird.frameAtual = incremento % baseRepeticao
+            }
+        },
         desenhar(){ // f(x) que desenha ele mesmo
+            flappyBird.atualizaFrameAtual()
+            const { spriteX, spriteY } = this.movimentos[flappyBird.frameAtual]
             contexto.drawImage(
                 sprites, // Local onde está a imagem de referencia
-                flappyBird.spriteX, flappyBird.spriteY, 
+                spriteX, spriteY, 
                 flappyBird.largura, flappyBird.altura, 
                 flappyBird.x, flappyBird.y,
                 flappyBird.largura, flappyBird.altura
@@ -61,31 +87,137 @@ function criaFlappyBird(){
     return flappyBird
 }
 
-const chao = {
-    spriteX: 0,
-    spriteY: 610, 
-    largura: 224,
-    altura: 112,
-    x: 0,
-    y: canvas.height - 112,
-    desenhar(){
-        contexto.drawImage(
-            sprites,
-            chao.spriteX, chao.spriteY, 
-            chao.largura, chao.altura, 
-            chao.x, chao.y,
-            chao.largura, chao.altura
-        )
-        
-        contexto.drawImage(
-            sprites,
-            chao.spriteX, chao.spriteY, 
-            chao.largura, chao.altura, 
-            (chao.x + chao.largura), chao.y,
-            chao.largura, chao.altura
-        )
+function criaChao(){
+    const chao = {
+        spriteX: 0,
+        spriteY: 610, 
+        largura: 224,
+        altura: 112,
+        x: 0,
+        y: canvas.height - 112,
+        atualiza(){
+            const movimentoChao = 1
+            const repeteEm = chao.largura / 2
+            const movimentacao = chao.x - movimentoChao
+
+            chao.x = movimentacao % repeteEm
+
+        },
+        desenhar(){
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY, 
+                chao.largura, chao.altura, 
+                chao.x, chao.y,
+                chao.largura, chao.altura
+            )
+            
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY, 
+                chao.largura, chao.altura, 
+                (chao.x + chao.largura), chao.y,
+                chao.largura, chao.altura
+            )
+        }
     }
+    return chao
 }
+
+function criaCanos(){
+    const canos = {
+        largura: 52,
+        altura: 400,
+        chao: {
+            spriteX: 0,
+            spriteY: 169,
+        },
+        ceu: {
+            spriteX: 52,
+            spriteY: 169,
+        },
+        espaco: 80,
+        desenhar() {
+            canos.pares.forEach(function(par) {
+              const yRandom = par.y;
+              const espacamentoEntreCanos = 90;
+        
+              const canoCeuX = par.x;
+              const canoCeuY = yRandom; 
+      
+              // [Cano do Céu]
+              contexto.drawImage(
+                sprites, 
+                canos.ceu.spriteX, canos.ceu.spriteY,
+                canos.largura, canos.altura,
+                canoCeuX, canoCeuY,
+                canos.largura, canos.altura,
+              )
+              
+              // [Cano do Chão]
+              const canoChaoX = par.x;
+              const canoChaoY = canos.altura + espacamentoEntreCanos + yRandom; 
+              contexto.drawImage(
+                sprites, 
+                canos.chao.spriteX, canos.chao.spriteY,
+                canos.largura, canos.altura,
+                canoChaoX, canoChaoY,
+                canos.largura, canos.altura,
+              )
+      
+              par.canoCeu = {
+                x: canoCeuX,
+                y: canos.altura + canoCeuY
+              }
+              par.canoChao = {
+                x: canoChaoX,
+                y: canoChaoY
+              }
+            })
+          },
+        colisao(par){
+            const cabecaDoFlappy = globais.flappyBird.y;
+            const peDoFlappy = globais.flappyBird.y + globais.flappyBird.altura;
+            
+            if((globais.flappyBird.x + globais.flappyBird.largura) >= par.x) {
+                if(cabecaDoFlappy <= par.canoCeu.y) {
+                  return true;
+                }
+        
+                if(peDoFlappy >= par.canoChao.y) {
+                  return true;
+                }
+            }
+            return false;
+        },
+        pares: [],
+        atualiza(){
+            const passouFrames = frame % 100 === 0
+            if(passouFrames){
+                canos.pares.push({
+                    x: canvas.width,
+                    y: -150 * (Math.random() + 1)
+                })
+            }
+
+            canos.pares.forEach(function(par){
+                par.x = par.x - 2
+
+                if(canos.colisao(par)){
+                    mudarTela(telas.inicio)
+                }
+
+                // remove canos que já passaram para não enxer a memoria do user
+                if(par.x + canos.largura <= 0) {
+                    canos.pares.shift()
+                }
+            })
+        }
+    }
+    return canos
+}
+
+// --------------------------------- Objetos estaticos -----------------------------
 
 const planoDeFundo = {
     spriteX: 390,
@@ -136,9 +268,7 @@ const menssagemGetReady = {
     }
 }
 
-const globais = {} // objeto que preenche tudo que for global
-// Telas contem as funções que cada tela ira usar
-let telaAtiva = {}
+// --------------------------------- Telas -----------------------------
 
 function mudarTela(novaTela){
     telaAtiva = novaTela
@@ -152,15 +282,19 @@ const telas = {
     inicio: {
         inicializa(){
             globais.flappyBird = criaFlappyBird()
+            globais.chao = criaChao()
+            globais.canos = criaCanos()
         },
         desenha(){
             planoDeFundo.desenhar()
-            chao.desenhar()
+            globais.chao.desenhar()
             globais.flappyBird.desenhar()
+            globais.canos.desenhar()
             menssagemGetReady.desenhar()
         },
         atualiza(){
-
+            globais.chao.atualiza()
+            
         },
         click(){
             mudarTela(telas.jogo)
@@ -169,7 +303,9 @@ const telas = {
     jogo: {
         desenha(){
             planoDeFundo.desenhar()
-            chao.desenhar()
+            
+            globais.canos.desenhar()
+            globais.chao.desenhar()
             globais.flappyBird.desenhar()
         },
         click(){
@@ -177,16 +313,20 @@ const telas = {
         },
         atualiza(){
             globais.flappyBird.atualiza()
+            globais.chao.atualiza()
+            globais.canos.atualiza()
         }
     }
 }
 
-// --------------------------------Desenhar------------------------------------------
+// --------------------------------Canvas------------------------------------------
 function loop(){
     telaAtiva.desenha()
     telaAtiva.atualiza()
 
     requestAnimationFrame(loop) // Função para criar um loop que forma os FPS na tela
+
+    frame += 1
 }
 
 window.addEventListener('click', function(){
